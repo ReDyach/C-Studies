6af83eeb46183ea9c539ae7a678cdfb2b784795b
DESKTOP-JBHPGVM\USER <karnen0120@gmail.com>
2022-04-15 20:25:04 +0900

    숫자 맞추기 프로그램(조건문)

---
+++ Study3/condition.c
@@ -0,0 +1,166 @@
+#include<stdio.h>
+#include<time.h>
+
+int main(void)
+{
+
+	//버스를 탄다고 가정, 학생 또는 일반인.
+	//일반인 : 20세 이상
+
+
+	/*
+	int age = 25;
+
+	//if (조건) {...} else {...} 
+	if (age >= 20) {
+		printf("일반인 입니다.");
+	}
+	else
+		printf("학생입니다."); 
+	*/
+
+	/*
+	//초등학생 (8~13) 중학생 (14~16) 고등학생 (17~19)
+	//if / else / else if
+	int age = 8;
+	if (age >= 8 && age <= 13) {
+		printf("초등학생 입니다.\n");
+	}
+	else if (age >= 14 && age <= 16) {
+		printf("중학생 입니다.\n");
+	}
+	else if (age >= 17 && age <= 19) {
+		printf("고등학생 입니다.\n");
+	}
+	else
+		printf("학생이 아닌가 봅니다.\n");
+	*/
+
+	/*
+	// break와 continue
+	// 1번부터 30번까지 있는 반, 1번부터 5번까지 조별 발표
+	for (int i = 1; i <= 30; i++) {
+		if (i >= 6) {
+			printf("나머지 학생은 집에 가세요.\n");
+			break; // i가 6 이상이 되면 for문 탈출
+		}
+		printf("%d번 학생은 조별 발표 준비를 하세요.\n", i);
+	}
+	*/
+
+
+	/*
+	// 1번부터 30번까지, 7번은 아파서 결석
+	// 7번 제외하고 6번부터 10번까지 발표
+	for (int i = 1; i <= 30; i++) 
+	{
+		if (i >= 6 && i <= 10) 
+		{
+			if (i == 7) 
+			{
+				printf("%d번 학생은 결석입니다.\n", i);
+				continue;
+			}
+			printf("%d번 학생은 조별 발표 준비를 하세요.\n", i);
+		}
+
+	}
+	*/
+
+	/*
+	// &&는 and, ||는 or
+	int a = 10;
+	int b = 10;
+	int c = 12;
+	int d = 13;
+	if (a == b || c == d)
+	{
+		printf("a와 b가 같거나 c와 d의 값이 같습니다.\n");
+	}
+	else
+		printf("값이 서로 다르네요.");
+
+	return 0;
+	*/
+
+	/*
+	// 가위 0 바위 1 보 2
+
+	srand(time(NULL)); //난수 초기화
+	int i = rand() % 3; // 0 ~ 2 수 반환.
+	if (i == 0)
+	{
+		printf("가위\n");
+	}
+	else if (i == 1)
+	{
+		printf("바위\n");
+	}
+	else if (i == 2)
+	{
+		printf("보\n");
+	}
+	else
+		printf("몰라용\n");
+
+	return 0;
+	*/
+
+	/*
+	//switch
+	srand(time(NULL)); //난수 초기화
+	int i = rand() % 3; // 0 ~ 2 수 반환.
+	switch (i)
+	{
+	case 0:
+		printf("가위\n");
+		break;
+	case 1:
+		printf("바위\n");
+		break;
+	case 2:
+		printf("보\n");
+		break;
+	dafault:
+		printf("몰라요\n");
+		break;
+	}
+	*/
+	// Up and Down
+	srand(time(NULL));
+	int num = rand() % 100 + 1; // 1~100 사이의 숫자
+	
+	// printf("숫자 = %d\n", num);
+	int answer = 0; // 정답
+	int chance = 5; // 기회
+
+	while (chance > 0)
+	{
+		printf("남은 기회 : %d\n", chance--);
+		printf("숫자를 맞춰보세요 (1~100) : ");
+		scanf_s("%d", &answer);
+
+		if (answer > num)
+		{
+			printf("DOWN ↓ \n\n");
+		}
+		else if (answer < num)
+		{
+			printf("UP ↑ \n\n");
+		}
+		else if (answer == num)
+		{
+			printf("정답입니다 ! \n\n");
+			break;
+		}
+		else
+			printf("알 수 없는 오류가 발생했습니다.\n\n");
+
+		if (chance == 0)
+		{
+			printf("모든 기회를 다 사용하셨네요. 아쉽게 실패했습니다!");
+			break;
+		}
+	}
+	
+}
